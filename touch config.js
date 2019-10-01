b93a851d6c320a3077b9c8cd92799ad37b82d2e7
breakpoint35 <56022298+breakpoint35@users.noreply.github.com>
2019-10-01 20:33:22 +0200

    Update touch config.js

--- touch config.js
+++ touch config.js
@@ -1,1 +1,7 @@
-
+<script type='text/javascript' src='config.js'></script>
+<script type='text/javascript' src='script.js></script>
+var config = {
+  MY_KEY : '123456',
+  SECRET_KEY : '56789',
+  KEY_2 : '101010'
+}
